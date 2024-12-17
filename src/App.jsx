3f1adafa3f873fc74a426513e4384f668ab05026
LudovicAns <ludovicanselin@gmail.com>
2024-12-17 10:35:48 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/containers/header/Header.jsx";
+import Footer from "./components/containers/footer/Footer.jsx";
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    )
+}
+
+export default App
---
+++ src/components/containers/header/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <nav>
+            <h1>My App</h1>
+        </nav>
+    );
+}
+
+export default Header;
