5e3a2f583d17fe325fdd7d61491f4ef6423429a0
LudovicAns <ludovicanselin@gmail.com>
2024-12-17 15:20:35 +0100

    refactor: replace User component with Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import Header from "./components/containers/header/Header.jsx";
 import Footer from "./components/containers/footer/Footer.jsx";
-import User from "./components/containers/user/User.jsx";
-import LastName from "./components/containers/user/LastName.jsx";
-import FirstName from "./components/containers/user/FirstName.jsx";
-import Hobbies from "./components/containers/user/Hobbies.jsx";
-import Button from "./components/ui/buttons/Button.jsx";
+import Player from "./components/containers/player/Player.jsx";
 import './App.css'
 
 function App() {
@@ -13,21 +9,7 @@ function App() {
             <Header/>
             <main>
                 <section>
-                    <User>
-                        <FirstName value="Ludovic"/>
-                        <LastName value="Anselin"/>
-                        <br/>
-                        <Hobbies value={[
-                            "Sport",
-                            "Eat",
-                            "Sleep"
-                        ]}/>
-                    </User>
-                    <Button content={"Click me"}
-                            style={{
-                                backgroundColor: "white",
-                                color: "black",
-                            }}/>
+                    <Player />
                 </section>
             </main>
             <Footer/>
