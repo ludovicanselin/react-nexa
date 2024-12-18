1c3de88aad0d7a2ccbee388075fd8f35a075bb47
LudovicAns <ludovicanselin@gmail.com>
2024-12-18 09:51:01 +0100

    refactor: remove App.css and integrate TailwindCSS
    Replaced the traditional CSS file with TailwindCSS for styling. This reduces redundant code and leverages utility-first CSS for a more streamlined approach. Necessary dependencies for TailwindCSS and PostCSS were added to package-lock.json.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/containers/header/Header.jsx";
 import Footer from "./components/containers/footer/Footer.jsx";
 import Player from "./components/containers/player/Player.jsx";
-import './App.css'
 
 function App() {
     return (
